d9f7f9c6237a50730e075dc2f3e9b2ba73b26c4c
Vfranco <vfrancov25@gmail.com>
2021-11-14 10:27:34 -0500

    Seccion 4

--- src/App.js
+++ src/App.js
@@ -1,31 +1,12 @@
-//import logo from './logo.svg';
-import './App.css';
+import Button from "./Button";
 
-const estiloTwo = {
-  boxShadow : '0 5px 3px rgba(0,0,0,0.1)'
-}
-
-const estilo = (bg = '#333') => ({
-  backgroundColor : bg,
-  color: '#fff',
-  padding : '10px 15px'
-})
-
-const Li = ({children}) => {
-  return(
-    <li className="clase-li" style={{...estiloTwo, ...estilo()}}>{children}</li>
-  )
-}
-
-//Esta es la declaracion de un componente
 const App = () => {
-  //Siempre debe retornar algo, asi sea vacio
-  //const valor = 'triste'; // Un equivalente a interpolación
-  return (
-    <ul className="clase-css">
-      <Li estado="feliz">Valor de li</Li>
-    </ul>
-  );
+    return (
+        <div>
+            <h1>Hola Mundo</h1>
+            <Button onClick={ () => alert('clikeado!') }>Enviar</Button>
+        </div>
+    )
 }
 
 export default App;
---
+++ src/intro-app.js
@@ -0,0 +1,32 @@
+//import logo from './logo.svg';
+//import './App.css';
+import './main.css';
+
+const estiloTwo = {
+  boxShadow : '0 5px 3px rgba(0,0,0,0.1)'
+}
+
+const estilo = (bg = '#333') => ({
+  backgroundColor : bg,
+  color: '#fff',
+  padding : '10px 15px'
+})
+
+const Li = ({children}) => {
+  return(
+    <li className="clase-li">{children}</li>
+  )
+}
+
+//Esta es la declaracion de un componente
+const App = () => {
+  //Siempre debe retornar algo, asi sea vacio
+  //const valor = 'triste'; // Un equivalente a interpolación
+  return (
+    <ul className="clase-css">
+      <Li estado="feliz">Valor de li</Li>
+    </ul>
+  );
+}
+
+export default App;
